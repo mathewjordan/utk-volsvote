52f35ebdba723de1ae7a40396f72ed24454f3284
Mat Jordan <mat@utk.edu>
2020-09-02 23:03:32 -0400

    Add google analytics tracking.

--- wp-content/themes/ut-thehill/library/partials/head.php
+++ wp-content/themes/ut-thehill/library/partials/head.php
@@ -20,7 +20,15 @@
   <?php  
   }else{  ?><?php } ?>
 
-<?php wp_head(); ?>
+    <?php wp_head(); ?>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177228627-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'UA-177228627-1');
+    </script>
 </head>
